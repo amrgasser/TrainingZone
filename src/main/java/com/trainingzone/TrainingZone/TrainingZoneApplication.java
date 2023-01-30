12b83e4d9def31b9c8604e5d541ebc06bda8a427
amrgasser <amr_gasser@hotmail.com>
2023-01-30 18:06:30 -0500

    initial commit

---
+++ src/main/java/com/trainingzone/TrainingZone/TrainingZoneApplication.java
@@ -0,0 +1,13 @@
+package com.trainingzone.TrainingZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainingzone/TrainingZone/TrainingZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingzone.TrainingZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
